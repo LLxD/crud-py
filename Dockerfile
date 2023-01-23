a8326f2e20ccc8f249a94ded9ee38c5f74c420e3
LL <limalucas454@gmail.com>
2023-01-23 20:25:11 -0300

    minor fix and improving print

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.7-alpine
 WORKDIR /app
 COPY . /app
+RUN touch /app/crud.db
 RUN pip install -r requirements.txt
 EXPOSE 5000
 CMD ["python3 crud.py"]
--- crud.py
+++ crud.py
@@ -188,6 +188,7 @@ if __name__ == '__main__':
     db.create_all()
     # if the db is empty, seed it
     if not Airport.query.all():
+        print("Seeding the database")
         seed_database()
     app.run(host="0.0.0.0",debug=True)
 
