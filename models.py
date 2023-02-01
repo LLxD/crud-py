46b44919c0bc96a5469c9bc108faa1456174e096
LL <limalucas454@gmail.com>
2023-02-01 09:39:05 -0300

    minor model adjustments

--- models.py
+++ models.py
@@ -19,11 +19,11 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(80), unique=True, nullable=False)
-    password = Column(String(120), unique=True, nullable=False)
+    password = Column(String(120), nullable=False)
     token = Column(String(120), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.email
+        return json.dumps(to_dict(self))
 
 
 class Airport(Base):
